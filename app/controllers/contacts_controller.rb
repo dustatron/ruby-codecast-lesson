073b03304204f522d23dd4d3633576c68f7f4f8a
dustatron <dustymccord@gmail.com>
2017-08-24 19:45:56 +0000

    Added Flash message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,21 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            flash[:success] = "Message Sent."
+            redirect_to new_contact_path, notice: "Message Sent."
+        else
+            flash[:error] = "Error Occured. Message was not sent."
+            redirect_to new_contact_path, notice: "Error!"
+        end
     end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
+  
 
 end
