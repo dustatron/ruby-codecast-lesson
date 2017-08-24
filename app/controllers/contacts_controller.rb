fb0a8ded2fdd6de8204ec22977a55ac33f3a939c
dustatron <dustymccord@gmail.com>
2017-08-24 22:31:37 +0000

    updated SendGris settings again

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -14,7 +14,7 @@ class ContactsController < ApplicationController
             ContactMailer.contact_email(name, email, body)
             
             
-            flash[:success] = "Message Sent."
+            flash[:success] = "Message≈ Sent."
             redirect_to new_contact_path, notice: "Message Sent."
         else
             flash[:danger] = "Error Occured. Message was not sent."
