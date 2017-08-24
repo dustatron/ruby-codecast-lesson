d9373e0eb55eb91a68a3f81b010b27aa57d5a2ec
dustatron <dustymccord@gmail.com>
2017-08-24 18:43:26 +0000

    fixed migrade bug - miss spelling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :contacts
-  get '/contact' => 'pages#contact'
+  get '/blog' => 'pages#blog'
   get '/about' => 'pages#about'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
