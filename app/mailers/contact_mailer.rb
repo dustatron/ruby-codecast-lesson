e115c4da83727ba0c4e85608f8fbb46267ee9bf0
dustatron <dustymccord@gmail.com>
2017-08-24 21:57:03 +0000

    updated default email address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ActionMailer::Base
     
-    default to: 'dustymccord@gmail.comments'
+    default to: 'dustymccord@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
