a1c031c4bb354d46e17e50ba718000aad6c9a73b
dustatron <dustymccord@gmail.com>
2017-10-04 21:16:08 +0000

    added tables to the db and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825024210) do
+ActiveRecord::Schema.define(version: 20171004205507) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,20 @@ ActiveRecord::Schema.define(version: 20170825024210) do
 
   add_index "models", ["email"], name: "index_models_on_email", unique: true
   add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+
+  create_table "plans", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
