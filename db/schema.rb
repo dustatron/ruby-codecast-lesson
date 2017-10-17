8d892d9852e30da416eaad2b3733b3cc6b457734
dustatron <dustymccord@gmail.com>
2017-10-17 20:11:04 +0000

    added modified controller for stripe.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005005120) do
+ActiveRecord::Schema.define(version: 20171017200333) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20171005005120) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "plan"
+    t.string   "stripe_customer_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
