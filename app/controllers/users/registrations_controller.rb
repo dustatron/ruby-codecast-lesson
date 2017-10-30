27ff0276ccd90a657c6c2d0d9b4d10fd3890b31a
dustatron <dustymccord@gmail.com>
2017-10-30 21:18:00 +0000

    added notice and styles

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   
+  before_filter :select_plan, only: :new
 
   
   def create
@@ -14,5 +15,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
+  
+  private
+  
+    def select_plan
+      unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please Select a Memebership Plan to Sign-up."
+        redirect_to root_url
+      end
+    end
   
 end
