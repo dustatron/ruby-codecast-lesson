680f0dfcb9180e9ee61521c9c0194485de52775f
dustatron <dustymccord@gmail.com>
2017-10-31 23:53:12 +0000

    added before_filter for authenticated_user for profile and users.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+    before_action :authenticate_user! #devise
+    before_action :only_current_user
     
     def new
         @user = User.find( params[:user_id] )
@@ -21,6 +23,19 @@ class ProfilesController < ApplicationController
         @user = User.find( params[:user_id] )
         @profile = @user.profile
     end
+    
+    def update
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+        
+        if @profile.update_attributes(profile_params)
+            flash[:success] = "Profile updated"
+            redirect_to user_path( params[:user_id] )
+        else
+            redirect_to action: :edit
+        end
+        
+    end
 
     
     private
@@ -28,6 +43,12 @@ class ProfilesController < ApplicationController
         def profile_params 
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
         end
+        
+        def only_current_user
+            @user = User.find( params[:user_id])
+            redirect_to(root_url) unless @user == current_user
+        end
+        
     
     
 
