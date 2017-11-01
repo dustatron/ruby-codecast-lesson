3ccebd5b7644d50f7e6f5cf44f1fad4402868626
dustatron <dustymccord@gmail.com>
2017-11-01 14:56:56 +0000

    added avatar colmn.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,8 @@
 class ProfilesController < ApplicationController
     before_action :authenticate_user! #devise
     before_action :only_current_user
+    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
     
     def new
         @user = User.find( params[:user_id] )
